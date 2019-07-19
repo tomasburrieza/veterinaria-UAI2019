730b3ba9f32c9ef7abd0042be743f939833fcdd4
tburrieza <tomas.burrieza@mercadolibre.com>
2019-07-19 17:17:00 -0300

    refactor on welcome page home controller index

--- UAIVeterinaria/Migrations/Configuration.cs
+++ UAIVeterinaria/Migrations/Configuration.cs
@@ -15,20 +15,18 @@ namespace UAIVeterinaria.Migrations
 
         protected override void Seed(UAIVeterinaria.Data.VetDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
-            DateTime fechaHoy = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, 0, 0);
+            // Establecemos una fecha para iniciar el seed de los horarios disponibles por veterinarie, sala y mascota.
+            DateTime todayDateToSeed = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, 0, 0);
             for (int i = 0; i < 10; i++)
             {
                 Appointment appointment = new Appointment();
-                appointment.PetId = 0;
-                appointment.RoomId = 0;
                 appointment.DoctorId = 0;
-                appointment.DateTime = fechaHoy.AddHours(i);
+                appointment.RoomId = 0;
+                appointment.PetId = 0;
+                appointment.DateTime = todayDateToSeed.AddHours(i);
                 context.Appointments.Add(appointment);
             }
+
             context.SaveChanges();
         }
     }
